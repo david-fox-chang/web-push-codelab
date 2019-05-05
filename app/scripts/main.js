755372a89a57cc22a047c498b8adddf4ffbd23a6
David <david.fox0904@gmail.com>
2019-05-05 23:51:48 +0800

    示範完成

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,7 +21,29 @@
 
 'use strict';
 
-const applicationServerPublicKey = '<Your Public Key>';
+const useKeyPairNum = 1;
+
+// 此 app 的 google 說明：向網絡應用添加推送通知
+// https://developers.google.com/web/fundamentals/codelabs/push-notifications
+
+// 可用此網頁產生的公私鑰隨意增加
+// https://web-push-codelab.glitch.me/
+
+const keyPairs = [
+  {
+    public: 'BP7o_CbrkRAnq6nIGNDdfDpqjoepJlFdB0Tonr7MnOaT0GXBdscRzFCC0-fRkKo53VRSSthmVZMLAQ1eojMcQSQ',
+    private: 'bhqTC8eUSoagjKfYFEL_PvL78Y3Mc-CVnxc16cJU1dU',
+  },
+  {
+    public: 'BOF5MsFjaBSTPMbGXoIi-DAR-jk2X5Zf8Je-5wFb5t1PcJ5C9Js4Riw4eFGJKzygj9HEr2NGbe96TZP5CSRPHvw',
+    private: '2C-AN1SEIXZShK38v93H0NwG1i5M8oPIXYTY-ND5H5Q',
+  },
+];
+
+const {
+  public: applicationServerPublicKey,
+  private: applicationServerPrivateKey,
+} = keyPairs[useKeyPairNum];
 
 const pushButton = document.querySelector('.js-push-btn');
 
@@ -42,3 +64,117 @@ function urlB64ToUint8Array(base64String) {
   }
   return outputArray;
 }
+
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push is supported');
+
+  navigator.serviceWorker.register('sw.js')
+  .then(function(swReg) {
+    console.log('Service Worker is registered', swReg);
+    swRegistration = swReg;
+    initialiseUI();
+  })
+  .catch(function(error) {
+    console.error('Service Worker Error', error);
+  });
+} else {
+  console.warn('Push messaging is not supported');
+  pushButton.textContent = 'Push Not Supported';
+}
+
+function initialiseUI() {
+
+  pushButton.addEventListener('click', function() {
+    pushButton.disabled = true;
+    if (isSubscribed) {
+      // TODO: Unsubscribe user
+      unsubscribeUser();
+    } else {
+      subscribeUser();
+    }
+  });
+
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    isSubscribed = !(subscription === null);
+
+    updateSubscriptionOnServer(subscription);
+
+    if (isSubscribed) {
+      console.log('User IS subscribed.');
+    } else {
+      console.log('User is NOT subscribed.');
+    }
+
+    updateBtn();
+  });
+}
+
+function updateBtn() {
+  if (isSubscribed) {
+    pushButton.textContent = 'Disable Push Messaging';
+  } else {
+    pushButton.textContent = 'Enable Push Messaging';
+  }
+
+  pushButton.disabled = false;
+}
+
+function subscribeUser() {
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  swRegistration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: applicationServerKey
+  })
+  .then(function(subscription) {
+    console.log('User is subscribed:', subscription);
+
+    updateSubscriptionOnServer(subscription);
+
+    isSubscribed = true;
+
+    updateBtn();
+  })
+  .catch(function(err) {
+    console.log('Failed to subscribe the user: ', err);
+    updateBtn();
+  });
+}
+
+function updateSubscriptionOnServer(subscription) {
+  // TODO: Send subscription to application server
+
+  const subscriptionJson = document.querySelector('.js-subscription-json');
+  const subscriptionDetails =
+    document.querySelector('.js-subscription-details');
+
+  if (subscription) {
+    subscriptionJson.textContent = JSON.stringify(subscription);
+    subscriptionDetails.classList.remove('is-invisible');
+  } else {
+    subscriptionDetails.classList.add('is-invisible');
+  }
+}
+
+function unsubscribeUser() {
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    if (subscription) {
+      // TODO: Tell application server to delete subscription
+      return subscription.unsubscribe();
+    }
+  })
+  .catch(function(error) {
+    console.log('Error unsubscribing', error);
+  })
+  .then(function() {
+    updateSubscriptionOnServer(null);
+
+    console.log('User is unsubscribed.');
+    isSubscribed = false;
+
+    updateBtn();
+  });
+}
+
